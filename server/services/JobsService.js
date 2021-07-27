d5a335d263d3146975cee4c701aeb4af922e49d3
Jonathan Wright <j.wright72127@gmail.com>
2021-07-27 15:31:09 -0600

    Added All functionality into Jobs

---
+++ server/services/JobsService.js
@@ -0,0 +1,32 @@
+import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
+
+class JobsService {
+  async getAll(query = {}) {
+    const jobs = await dbContext.Job.find(query)
+    return jobs
+  }
+
+  async getById(id) {
+    const job = await dbContext.Job.findById(id)
+    if (!id) {
+      throw new BadRequest('Invalid Id')
+    }
+    return job
+  }
+
+  async create(body) {
+    const job = await dbContext.Job.create(body)
+    return job
+  }
+
+  async destroy(id) {
+    const job = await dbContext.Job.findByIdAndDelete(id)
+    if (!job) {
+      throw new BadRequest('Invalid Id')
+    }
+    return job
+  }
+}
+
+export const jobsService = new JobsService()
